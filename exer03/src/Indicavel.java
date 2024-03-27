9f2046188388d6e0617a92be715e8cdd4bdd749d
Gabriel Rodrigues <gabriell.dorodrigues@gmail.com>
2024-03-27 19:46:11 -0300

    Delete exer03 directory

--- exer03/src/Indicavel.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public interface Indicavel {
-    Boolean getElegivel();
-    Short getNumeroDeIndicacoes();
-    void setElegivel(Boolean elegivel);
-    void setNumeroDeIndicacoes(Short ni);
-}
